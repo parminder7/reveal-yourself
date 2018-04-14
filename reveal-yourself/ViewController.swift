b353202d93ba368ce8859966c3e0d8da63c67f0f
Parminder Kaur <parminder_jk@hotmail.com>
2018-04-14 13:35:28 -0400

    Initial Commit

---
+++ reveal-yourself/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  reveal-yourself
+//
+//  Created by Parminder Kaur on 2018-04-14.
+//  Copyright © 2018 Parminder Kaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
